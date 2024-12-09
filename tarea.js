150f4f0767dd82b23af0cdc9c6b39d1dff680751
Luis Alburez <alburezluis@gmail.com>
2024-12-09 10:01:26 -0600

    commit para subir los archivos

---
+++ tarea.js
@@ -0,0 +1,6 @@
+// CREA EL ELEMENTO NOMBRE, LE ASIGNA EL TEXTO Y LO AGREGA AL document.body USANDO MANIPULACIÓN DEL DOM
+let nombre = document.createElement("h1");
+nombre.innerText = "Luis Alejandro Alburez Samayoa";
+nombre.style.background = "blue";
+nombre.style.color = "yellow";
+document.body.append(nombre);
